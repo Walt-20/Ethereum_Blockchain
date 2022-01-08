5e7c60a0266c7d7798db6a8e1d37c735e0fdd582
Rubiks12 <AHwl77@gmail.com>
2022-01-08 15:23:23 -0700

    Had to add const HelloWorld to 1_initial_migration.js /Works now.

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -3,3 +3,10 @@ const Migrations = artifacts.require("Migrations");
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
 };
+
+const HelloWorld = artifacts.require("HelloWorld");
+
+module.exports = function (deployer) {
+  deployer.deploy(Migrations);
+  deployer.deploy(HelloWorld);
+  };
